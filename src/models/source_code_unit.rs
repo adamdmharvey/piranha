6baec40428eece3a611d7bd2a70b6ab674550548
Ameya Ketkar <94497232+ketkarameya@users.noreply.github.com>
2023-02-23 10:48:18 -0800

    Bug/Feature: Handle syntactically incorrect input trees
    ghstack-source-id: eb8d3100dc56f71243c455b2f192de2251d5e51d
    Pull Request resolved: https://github.com/uber/piranha/pull/386

--- src/models/source_code_unit.rs
+++ src/models/source_code_unit.rs
@@ -70,7 +70,7 @@ impl SourceCodeUnit {
     piranha_arguments: &PiranhaArguments,
   ) -> Self {
     let ast = parser.parse(&code, None).expect("Could not parse code");
-    Self {
+    let source_code_unit = Self {
       ast,
       code,
       substitutions: substitutions.clone(),
@@ -78,7 +78,14 @@ impl SourceCodeUnit {
       rewrites: Vec::new(),
       matches: Vec::new(),
       piranha_arguments: piranha_arguments.clone(),
+    };
+    // Panic if allow dirty ast is false and the tree is syntactically incorrect
+    if !piranha_arguments.allow_dirty_ast() && source_code_unit._number_of_errors() > 0 {
+      error!("{}: {}", "Syntax Error".red(), path.to_str().unwrap().red());
+      _ = &source_code_unit._panic_for_syntax_error();
     }
+
+    source_code_unit
   }
 
   pub(crate) fn root_node(&self) -> Node<'_> {
@@ -339,17 +346,31 @@ impl SourceCodeUnit {
     // Get the tree_sitter's input edit representation
     let (new_source_code, ts_edit) = get_tree_sitter_edit(self.code.clone(), &edit_to_apply);
     // Apply edit to the tree
+    let number_of_errors = self._number_of_errors();
     self.ast.edit(&ts_edit);
     self._replace_file_contents_and_re_parse(&new_source_code, parser, true);
-    if self.ast.root_node().has_error() {
-      let msg = format!(
-        "Produced syntactically incorrect source code {}",
-        self.code()
-      );
-      error!("{}", msg);
-      panic!("{}", msg);
+
+    // Panic if the number of errors increased after the edit
+    if self._number_of_errors() > number_of_errors {
+      self._panic_for_syntax_error();
     }
     ts_edit
+  }
+
+  fn _panic_for_syntax_error(&self) {
+    let msg = format!(
+      "Produced syntactically incorrect source code {}",
+      self.code()
+    );
+    error!("{}", msg);
+    panic!("{}", msg);
+  }
+
+  /// Returns the number of errors in the AST
+  fn _number_of_errors(&self) -> usize {
+    traverse(self.root_node().walk(), Order::Post)
+      .filter(|node| node.has_error())
+      .count()
   }
 
   /// Deletes the trailing comma after the {deleted_range}
